386fc576e1f106a611c5aee8b01b7e114b19a059
ARNAV CHENNAVARAM <arnav@ARNAVs-MacBook.local>
2025-07-07 17:37:56 +0530

    Remove build folder and update .gitignore

--- frontend/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_BugTracking_FILE /Users/arnav/Documents/projects/git projects/BUG-TRACKING-SYSTEM/frontend/build/BugTracking.app/Contents/MacOS/BugTracking)
-set(__QT_DEPLOY_TARGET_BugTracking_TYPE EXECUTABLE)
--- frontend/build/CMakeFiles/BugTracking.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BugTracking.
-# This may be replaced when dependencies are built.
